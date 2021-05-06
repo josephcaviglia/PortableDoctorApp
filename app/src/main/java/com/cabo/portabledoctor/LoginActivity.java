5010432af8af9d559590a9f78c3bdffa1fcde0a5
Joseph <joseph.caviglia@gmail.com>
2021-05-06 12:53:31 +0200

    Login

--- app/src/main/java/com/cabo/portabledoctor/LoginActivity.java
+++ app/src/main/java/com/cabo/portabledoctor/LoginActivity.java
@@ -6,9 +6,17 @@ import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 public class LoginActivity extends AppCompatActivity {
     EditText username, password;
     Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +33,34 @@ public class LoginActivity extends AppCompatActivity {
                 String user = username.getText().toString();
                 String pass = password.getText().toString();
 
+                try {
+                    URL url = new URL("https://portable-doctor.herokuapp.com/utente");
+                    HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setRequestProperty("Content-Type", "application/json; utf-8");
+                    con.setRequestProperty("Accept", "application/json");
+                    con.setDoOutput(true);
+                    String jsonInputString = "{email: "+user+", +password: "+pass+"}";
+                    try(OutputStream os = con.getOutputStream()) {
+                        byte[] input = jsonInputString.getBytes("utf-8");
+                        os.write(input, 0, input.length);
+                    }
+                    try(BufferedReader br = new BufferedReader(
+                            new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                        StringBuilder response = new StringBuilder();
+                        String responseLine = null;
+                        while ((responseLine = br.readLine()) != null) {
+                            response.append(responseLine.trim());
+                        }
+                        System.out.println(response.toString());
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("1");
+                }
+
                 if(user.equals("")||pass.equals(""))
-                    Toast.makeText(LoginActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Deficente", Toast.LENGTH_SHORT).show();
                 else{
                     if(user.equals("joseph") && pass.equals("123")){
                         Toast.makeText(LoginActivity.this, "Sign in successfull", Toast.LENGTH_SHORT).show();
