34620931eeae6fa73e310584a5d53f49b050a95b
Joseph <joseph.caviglia@gmail.com>
2021-05-08 19:07:52 +0200

    Little update

--- app/src/main/java/com/cabo/portabledoctor/LoginActivity.java
+++ app/src/main/java/com/cabo/portabledoctor/LoginActivity.java
@@ -2,8 +2,6 @@ package com.cabo.portabledoctor;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.Request;
