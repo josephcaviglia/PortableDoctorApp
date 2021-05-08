3e43be4cb574ee8b01b9a7776d55e1c319bdcddd
Joseph <joseph.caviglia@gmail.com>
2021-05-08 12:43:54 +0200

    Login

--- app/src/main/java/com/cabo/portabledoctor/LoginActivity.java
+++ app/src/main/java/com/cabo/portabledoctor/LoginActivity.java
@@ -18,9 +18,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
-        login = (Button) findViewById(R.id.login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
 
         RequestQueue queue = Volley.newRequestQueue(this);
         login.setOnClickListener(view -> {
