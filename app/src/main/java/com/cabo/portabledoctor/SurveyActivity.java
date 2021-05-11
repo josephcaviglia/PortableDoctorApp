abba97cf4caccacd5314fc2f2b0cc9931053cf3c
Joseph <joseph.caviglia@gmail.com>
2021-05-11 12:54:08 +0200

    Survey

--- app/src/main/java/com/cabo/portabledoctor/SurveyActivity.java
+++ app/src/main/java/com/cabo/portabledoctor/SurveyActivity.java
@@ -1,58 +1,60 @@
 package com.cabo.portabledoctor;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-public class RegisterActivity  extends AppCompatActivity {
-    Button signup;
-    EditText email, password, name, surname;
+public class SurveyActivity extends AppCompatActivity {
+    Button answer;
+    EditText tests;
+    RadioButton range1;
     TextView error;
-    RadioButton yes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        signup = findViewById(R.id.signup);
-        name = findViewById(R.id.name2);
-        surname = findViewById(R.id.surname2);
-        password = findViewById(R.id.password2);
-        email = findViewById(R.id.email2);
-        error = findViewById(R.id.error2);
-        yes = findViewById(R.id.radioYes);
+        setContentView(R.layout.activity_survey);
+        answer = findViewById(R.id.answer);
+        tests = findViewById(R.id.tests);
+        range1 = findViewById(R.id.radio1);
+        error = findViewById(R.id.error3);
+
+        SharedPreferences preferences = getSharedPreferences("keepLogged", MODE_PRIVATE);
+        String token = preferences.getString("token", "");
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        signup.setOnClickListener(view -> {
-            String name2 = name.getText().toString();
-            String surname2 = surname.getText().toString();
-            String email2 = email.getText().toString();
-            String password2 = password.getText().toString();
-            if("".equals(email2) || "".equals(password2) || "".equals(name2) || "".equals(surname2))
+
+        answer.setOnClickListener(view -> {
+            String tests2 = tests.getText().toString();
+            if("".equals(tests2))
                 error.setText(getResources().getString(R.string.all_fields));
             else {
                 boolean check = false;
-                if(yes.isChecked())
+                if(range1.isChecked())
                     check = true;
-                String url = "http://portable-doctor.herokuapp.com/utente?nome="+name2+"&cognome="+surname2+"&email="+email2+"&password="+password2+"&pazienteWarfarin="+check;
+                String url = "http://portable-doctor.herokuapp.com/utente?token="+token;
                 StringRequest postRequest = new StringRequest(Request.Method.POST, url, response -> {
                     if(response.equals("Errore"))
                         error.setText(getResources().getString(R.string.already));
                     else{
-                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                        intent.putExtra("SUCCESS", getResources().getString(R.string.success));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                         startActivity(intent);
                     }
                 }, err -> error.setText(getResources().getString(R.string.not_available)));
                 queue.add(postRequest);
             }
         });
+
     }
 }
